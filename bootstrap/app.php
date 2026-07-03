7303ece9d242e54b9484d956c5d2d91881d3f70b
tangosierra111 <taruna.tiofan11@gmail.com>
2026-07-04 01:23:46 +0700

    fix(api): reorder middleware and controller imports for consistency

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Middleware\VerifyFirebaseToken;
 use App\Http\Middleware\EnsureContentManager;
+use App\Http\Middleware\VerifyFirebaseToken;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
